35e21e448abbf81a25a0e0f19d5b1bce5d4ab003
RafaDSan <rafaeldss12@gmail.com>
2024-10-01 20:56:35 -0300

    feat: logic and documentation for CRUD operations

--- src/controllers/ReviewController.ts
+++ src/controllers/ReviewController.ts
@@ -1,21 +1,37 @@
 import { Request, Response } from "express";
 import { PreReview } from "../models/Review";
 import { User } from "../models/User";
-import { HttpCode } from "../core/constants";
+import { HttpCode, CreatePreReviewRequest, EthereumAddress } from "../core/constants";
 
+/**
+ * Class to manage the CRUD operations for the pre-review model
+ */
 export class ReviewController {
-	async createPreReview(req: Request, res: Response): Promise<void> {
+	/**
+	 * Function to create a pre-review document in MongoDB
+	 * @param req - Express request object containing the pre-review data
+	 * @param res - Express response object to send the pre-review document
+	 * @returns {Promise<void>} - Promise that resolves when the pre-review document is created
+	 */
+	async createPreReview(
+		req: Request<{}, {}, CreatePreReviewRequest & { programId: number }>,
+		res: Response
+	): Promise<void> {
 		try {
-			const { preReviewAnswers, connectedUserAddress } = req.body;
+			const { preReviewAnswers, connectedUserAddress, programId } = req.body;
 			const user = await User.findOne({ connectedUserAddress });
+
 			if (!user) {
 				res.status(HttpCode.NOT_FOUND).json({ message: "User not found" });
 				return;
 			}
+
 			const newPreReview = new PreReview({
 				user: user._id,
-				preReviewAnswers
+				preReviewAnswers,
+				programId
 			});
+
 			await newPreReview.save();
 			res.status(HttpCode.CREATED).json(newPreReview);
 		} catch (error) {
@@ -23,14 +39,22 @@ export class ReviewController {
 		}
 	}
 
-	async getPreReview(req: Request, res: Response): Promise<void> {
+	/**
+	 * Function to get the pre-review documents for a user.
+	 * @param req - Express request object containing the user's connected user address
+	 * @param res - Express response object returning the status code and the pre-review documents
+	 * @returns {Promise<void>} - Promise that resolves when the pre-review documents are fetched
+	 */
+	async getPreReview(req: Request<{ connectedUserAddress: EthereumAddress }>, res: Response): Promise<void> {
 		try {
 			const { connectedUserAddress } = req.params;
 			const user = await User.findOne({ connectedUserAddress });
+
 			if (!user) {
 				res.status(HttpCode.NOT_FOUND).json({ message: "User not found" });
 				return;
 			}
+
 			const preReviews = await PreReview.find({ user: user._id });
 			res.json(preReviews);
 		} catch (error) {
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,11 +2,21 @@ import { Request, Response } from "express";
 import { User } from "../models/User";
 import { HttpCode } from "../core/constants";
 
+/**
+ * Class to manage the CRUD operations for the user model
+ */
 export class UserController {
+	/**
+	 * Function to create a user document in MongoDB
+	 * @param req - Express request object containing the user data
+	 * @param res - Express response object to send the user document
+	 * @returns {Promise<void>} - Promise that resolves when the user document is created
+	 */
 	async createUser(req: Request, res: Response): Promise<void> {
 		try {
 			const { connectedUserAddress } = req.body;
 			const user = new User({ connectedUserAddress });
+
 			await user.save();
 			res.status(HttpCode.CREATED).json(user);
 		} catch (error) {
@@ -14,13 +24,21 @@ export class UserController {
 		}
 	}
 
+	/**
+	 * Function to get a user document in MongoDB
+	 * @param req - Express request object containing the user data
+	 * @param res - Express response object to send the user document
+	 * @returns {Promise<void>} - Promise that resolves when the user document is created
+	 */
 	async getUser(req: Request, res: Response): Promise<void> {
 		try {
 			const { connectedUserAddress } = req.params;
 			const user = await User.findOne({ connectedUserAddress });
+
 			if (!user) {
 				res.status(HttpCode.NOT_FOUND).json({ message: "User not found" });
 			}
+
 			res.json(user);
 		} catch (error) {
 			res.status(HttpCode.BAD_REQUEST).json({ message: "Error fetching user", error });
