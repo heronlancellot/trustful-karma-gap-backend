cb775327c0fb84b8703246fb1a6375cf24de2eda
RafaDSan <rafaeldss12@gmail.com>
2024-09-30 22:03:50 -0300

    feat: updated constants

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -14,3 +14,24 @@ export enum HttpCode {
 	NOT_FOUND = 404,
 	INTERNAL_SERVER_ERROR = 500
 }
+
+export enum Category {
+	DevTooling = "Dev tooling",
+	Education = "Education",
+	MarketingAndGrowth = "Marketing and Growth",
+	DeFi = "DeFi",
+	DAOsAndGovernance = "DAOs and Governance",
+	Community = "Community",
+	Gaming = "Gaming",
+	PublicGoods = "Public Goods",
+	ZKAndPrivacy = "ZK and privacy",
+	Other = "Other"
+}
+
+export enum ReceivedGrant {
+	Yes = "Yes, I got approved",
+	No = "No",
+	Pending = "I don't have the answer yet"
+}
+
+export type EthereumAddress = `0x${string}`;
