f1710241339cb5bbbfca57c0a55fceda6d5dfa1c
RafaDSan <rafaeldss12@gmail.com>
2024-09-30 22:03:28 -0300

    refactor: updating preReviewAnswers object

--- src/models/Review.ts
+++ src/models/Review.ts
@@ -1,14 +1,20 @@
-import { create } from "domain";
 import mongoose from "mongoose";
+import { Category, ReceivedGrant } from "../core/constants";
 
 const preReviewSchema = new mongoose.Schema({
 	user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 	preReviewAnswers: {
-		category: { type: String, required: true },
-		receivedGrant: { type: String, required: true }
+		category: { type: String, enum: Object.values(Category), required: true },
+		otherCategoryDescriptions: {
+			type: String,
+			required: function (this: { category: string }) {
+				return this.category === Category.Other;
+			}
+		},
+		receivedGrant: { type: String, enum: Object.values(ReceivedGrant), required: true }
 	},
-	createdAt: Date,
-	updatedAt: Date
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now }
 });
 
 export const PreReview = mongoose.model("PreReview", preReviewSchema);
