61f716a203d7ad5711a247bf5cc37732c6b58bfa
FrancoAguzzi <frankind.eth@gmail.com>
2024-10-11 14:25:24 -0300

    fix: add host definition in app listen

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ export class Server {
 
 		this.app.use(errorHandler);
 
-		this.app.listen(this.port, () => {
+		this.app.listen(this.port, "0.0.0.0", () => {
 			console.log(`Server running on port ${this.port}...`);
 		});
 	}
